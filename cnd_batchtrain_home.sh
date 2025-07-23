14ed11be37fad6b8433e08060fa0a2bc887a293b
azzarelli <aazzarellib@gmail.com>
2025-07-23 16:47:29 +0100

    Updated the tuning folder

--- cnd_batchtrain_home.sh
+++ cnd_batchtrain_home.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Assign the first argument to a variable
-EXP_NAME=final1
+EXP_NAME=unifieddyn
 ARGS=default.py
 
 # SAVEDIR=Pony
@@ -14,6 +14,13 @@ ARGS=default.py
 
 SAVEDIR=Bassist
 TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 python gui.py -s /media/barry/56EA40DEEA40BBCD/DATA/Condense/$SAVEDIR/ --expname "Condense/$SAVEDIR/$EXP_NAME" --configs arguments/Condense/default.py --test_iterations 1000
+
+SAVEDIR=Pianist
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 python gui.py -s /media/barry/56EA40DEEA40BBCD/DATA/Condense/$SAVEDIR/ --expname "Condense/$SAVEDIR/$EXP_NAME" --configs arguments/Condense/default.py --test_iterations 1000
+
+SAVEDIR=Fruit
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 python gui.py -s /media/barry/56EA40DEEA40BBCD/DATA/Condense/$SAVEDIR/ --expname "Condense/$SAVEDIR/$EXP_NAME" --configs arguments/Condense/default.py --test_iterations 1000
+
 
 # SAVEDIR=Curling
 # TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 python gui.py -s /media/barry/56EA40DEEA40BBCD/DATA/Condense/$SAVEDIR/ --expname "Condense/$SAVEDIR/$EXP_NAME" --configs arguments/Condense/default.py --test_iterations 1000
